2802b852fef8273500cbe31a0d302792de3287a5
raorabi12 <raorabi@askussolution.tech>
2024-06-21 17:48:16 +0500

    second day task 1 completed about react native core compoenets

--- App.tsx
+++ App.tsx
@@ -5,113 +5,209 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useCallback, useState} from 'react';
 import {
+  ActivityIndicator,
+  Button,
+  ImageBackground,
+  FlatList,
   SafeAreaView,
+  Text,
+  Modal,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Pressable,
+  RefreshControl,
   ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  Switch,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
+// import carsData from './Data/cars.json';
+interface Car {
+  id: string; // Assuming 'id' is a unique string identifier
   title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+  description: string;
 }
+const carsData: Car[] = require('./Data/cars.json');
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  // states
+  const [loading, setLoading] = useState(true);
+  const [selectedCar, setSelectedCar] = useState<string>();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // function to show activity indicator
+  (function () {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  })();
+
+  // function to open the modal
+  const onPressButton = () => {
+    setModalVisible(true);
   };
 
+  // function to change properties of selected item
+  const handleSelectedItem = (item: Car) => {
+    setSelectedCar(item.id);
+  };
+
+  // functiont to refresh the list
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <StatusBar animated={true} hidden={true} />
+      {loading ? (
+        <View>
+          <Text>Welcome to my App</Text>
+          {/* indicator to display loading */}
+          <ActivityIndicator size="large" color="#0000ff" />
+          <Text>Please wait a moment.</Text>
         </View>
-      </ScrollView>
+      ) : (
+        <SafeAreaView>
+          <ScrollView
+            // to refresh site
+            refreshControl={
+              <RefreshControl
+                tintColor="blue"
+                refreshing={refreshing}
+                onRefresh={onRefresh}
+              />
+            }>
+            {/* <Text>Hello! This is my first app</Text> */}
+
+            <Text>Click on button to view modal</Text>
+
+            {/* switch button to open modal */}
+            <Switch
+              trackColor={{true: 'pink', false: 'orange'}}
+              thumbColor={modalVisible ? 'blue' : 'red'}
+              value={modalVisible}
+              onValueChange={onPressButton}
+            />
+
+            {/* modal code */}
+            <Modal
+              visible={modalVisible}
+              presentationStyle="fullScreen"
+              animationType="slide">
+              <View style={styles?.modal}>
+                <Text>This is Modal on full screen</Text>
+                <Pressable
+                  style={styles?.closeButton}
+                  onPress={() => setModalVisible(false)}>
+                  <Text>Close</Text>
+                </Pressable>
+              </View>
+            </Modal>
+
+            {/* button to open modal */}
+            <Button
+              color="green"
+              title="Open Modal"
+              nextFocusUp={23}
+              onPress={onPressButton}
+            />
+          </ScrollView>
+          {/* FlatList of car items along with image and background image, futrthermore we can also use section list here and virualizedlist also  */}
+          <FlatList
+            data={carsData}
+            renderItem={({item}) => {
+              const backgroundColor =
+                item.id === selectedCar ? '#6e3b6e' : '#f9c23c';
+              const color = item.id === selectedCar ? 'white' : 'black';
+              return (
+                <ImageBackground
+                  source={require('./assets/beautiful-blurred-green-nature-background-ai-generated-photo.jpg')}>
+                  {/* to remove opactiy effect here we can use TouchableHeighlight */}
+                  <TouchableOpacity onPress={() => handleSelectedItem(item)}>
+                    <View style={[styles?.items, {backgroundColor}]}>
+                      <Image
+                        style={styles?.itemImage}
+                        alt="Car Image"
+                        // source={require('./assets/0x0.jpg')}
+                        source={{
+                          uri: 'https://media.architecturaldigest.com/photos/63079fc7b4858efb76814bd2/16:9/w_1920%2Cc_limit/9.%2520DeLorean-Alpha-5%2520%255BDeLorean%255D.jpg',
+                        }}
+                      />
+                      <View>
+                        <Text style={[styles?.itemTitle, {color}]}>
+                          {item?.title}
+                        </Text>
+                        <Text
+                          numberOfLines={3}
+                          ellipsizeMode="tail"
+                          style={[styles?.itemDescription, {color}]}>
+                          {item?.description}
+                        </Text>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                </ImageBackground>
+              );
+            }}
+            keyExtractor={(item: Car) => item.id}
+            extraData={selectedCar}
+          />
+        </SafeAreaView>
+      )}
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  items: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+    borderRadius: 10,
+    padding: 35,
+    margin: 10,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  itemTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  itemDescription: {
+    fontSize: 16,
+    marginTop: 5,
+    width: 250,
   },
-  highlight: {
-    fontWeight: '700',
+  itemImage: {
+    width: 100,
+    height: 100,
+    resizeMode: 'cover',
+    borderRadius: 10,
+  },
+  modal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    padding: 20,
+    borderRadius: 10,
+  },
+  closeButton: {
+    backgroundColor: '#f1c43c',
+    padding: 10,
+    borderRadius: 10,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
